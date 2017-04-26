7376d34bb265499be9319b02cca12436c2e2cfdd
Snedecor <ssnedecor@CharlotteNC.gov>
2017-04-26 14:49:02 -0400

    added response

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
  socket.on('chat message', function(msg){
     io.emit('chat message', 'You said: ' + msg);
+    io.emit('chat message', 'That is interesting. Tell me more.');
  });
 });
 
